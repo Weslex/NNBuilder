416810551be1be701351c25a6a0ad625f9a36094
Weslex <101282220+Weslex@users.noreply.github.com>
2023-01-28 05:02:26 -0600

    Early Morning Commit

--- src/generator.js
+++ src/generator.js
@@ -1,1 +1,18 @@
-var input_demension = document.getElementById("InputDem").value; 
+function allowDrop(ev) {
+    ev.preventDefault(); 
+}
+
+function drag(ev){
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data).cloneNode(true)); 
+}
+
+function generate(){
+    var input_demension = document.getElementById("InputDem").value;
+    document.getElementById("test").innerHTML = input_demension; 
+}
